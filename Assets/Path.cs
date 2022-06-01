4c5145c0419f959f46b59b0f8739db55f216fb5e
Michael Odermatt <43145047+MichaelOdermatt@users.noreply.github.com>
2022-05-31 18:57:11 -0600

    circle moves between two points

--- Assets/Path.cs
+++ Assets/Path.cs
@@ -4,5 +4,32 @@ using UnityEngine;
 
 public class Path : MonoBehaviour
 {
-    public List<Shape> Shapes;
+    public GameObject player;
+
+    public List<Shape> shapes;
+    private List<Transform> nodes;
+
+    private List<Transform> betweenNodes;
+    private float distanceAlongEdge = 0f;
+    private float movementSpeed = 0.05f;
+
+    private void Start()
+    {
+        nodes = new List<Transform>();
+        foreach (var shape in shapes)
+        {
+            nodes.AddRange(shape.controlPoints);
+        }
+
+        betweenNodes = new List<Transform>();
+        betweenNodes.Add(nodes[0]);
+        betweenNodes.Add(nodes[1]);
+    }
+
+    private void Update()
+    {
+        distanceAlongEdge += Time.deltaTime * movementSpeed;
+        if (betweenNodes.Count > 0)
+            player.transform.position = Vector3.Lerp(betweenNodes[0].position, betweenNodes[1].position, distanceAlongEdge);
+    }
 }
