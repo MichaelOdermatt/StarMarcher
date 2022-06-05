888cf7e9bc7f5da8f5da8531bf772ef5779e6c1d
Michael Odermatt <43145047+MichaelOdermatt@users.noreply.github.com>
2022-06-05 13:45:49 -0600

    added ability to hinge from a node

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 [RequireComponent(typeof(Rigidbody2D))]
@@ -14,21 +12,33 @@ public class Player : MonoBehaviour
     private float launchForce = 250f;
     private Rigidbody2D PlayerRigidBody;
     public Transform nodeTransform;
+    public Camera Camera;
     private float angle = 0;
+
+    public HingeJoint2D hinge;
 
     private void Start()
     {
         PlayerRigidBody = GetComponent<Rigidbody2D>();
+        hinge.enabled = false;
     }
 
+    // TODO used fixed update instead
     private void Update()
     {
+        bool clicked = Input.GetButtonDown("Fire1");
+
         if (nodeTransform != null)
         {
             RotateAroundNode();
-            if (Input.GetButtonDown("Fire1"))
+            if (clicked)
                 Launch();
-        } 
+        }
+        else
+        {
+            if (clicked)
+                HingeToNode();
+        }
     }
 
     private void RotateAroundNode()
@@ -47,7 +57,7 @@ public class Player : MonoBehaviour
         Vector2 launchVector = CalculateLaunchVector();
         nodeTransform = null;
         PlayerRigidBody.isKinematic = false;
-        PlayerRigidBody.AddForce(launchVector * launchForce); 
+        PlayerRigidBody.AddForce(launchVector * launchForce);
     }
 
     private Vector2 CalculateLaunchVector()
@@ -55,12 +65,44 @@ public class Player : MonoBehaviour
         return PlayerRigidBody.transform.position - nodeTransform.position;
     }
 
+    private void HingeToNode()
+    {
+        if (hinge.enabled == true)
+        {
+            RemoveHinge();
+            return;
+        }
+
+        var clickLocation = Camera.ScreenToWorldPoint(Input.mousePosition);
+        RaycastHit2D hit = Physics2D.Raycast(clickLocation, Vector2.zero);
+
+        if (hit == false)
+            return;
+
+        SetHinge(hit);
+    }
+
+    private void RemoveHinge()
+    {
+        hinge.anchor = Vector2.zero;
+        hinge.enabled = false;
+    }
+
+    private void SetHinge(RaycastHit2D hit)
+    {
+        hinge.enabled = true;
+        var node = hit.collider.gameObject;
+        hinge.anchor = transform.InverseTransformPoint(node.transform.position);
+    }
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (nodeTransform != null)
             return;
 
+        if (hinge.enabled)
+            RemoveHinge();
+
         nodeTransform = collision.collider.transform;
     }
-
 }
