bd7f65f045c73c7a15a71ec0a2bb3b65a176510b
Michael Odermatt <43145047+MichaelOdermatt@users.noreply.github.com>
2022-07-20 14:44:54 -0600

    added grapple attach sound

--- Assets/Scripts/Player/PlayerGrapple.cs
+++ Assets/Scripts/Player/PlayerGrapple.cs
@@ -20,8 +20,9 @@ public class PlayerGrapple : MonoBehaviour
     public bool IsDrawingGrapple = false;
     public ParticleSystem GrappleParticleSystem;
 
+    [SerializeField]
+    private AudioSource GrappleAttachSound;
     private Coroutine DrawThenSetGrappleCoroutine;
-
     private Rigidbody2D PlayerRigidBody;
     private LineRenderer LineRenderer;
     private HingeJoint2D Hinge;
@@ -108,6 +109,7 @@ public class PlayerGrapple : MonoBehaviour
         LineRenderer.SetPosition(1, point);
         SetHinge(hit.collider.gameObject);
         PlayGrappleParticle(point);
+        PlayGrappleAttachSound();
 
         IsDrawingGrapple = false;
     }
@@ -134,6 +136,12 @@ public class PlayerGrapple : MonoBehaviour
             GrappleParticleSystem.transform.position = position;
             GrappleParticleSystem.Play();
         }
+    }
+
+    private void PlayGrappleAttachSound()
+    {
+        if (GrappleAttachSound != null)
+            GrappleAttachSound.Play();
     }
 
     /// <summary>
